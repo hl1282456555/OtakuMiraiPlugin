aa48f3ccd2278ece7f83c857133b3657afe32bbe
Luo Yu <hl1282456555@outlook.com>
2022-08-05 09:31:35 +0800

    update

--- Source/Public/MiraiCP.hpp
+++ Source/Public/MiraiCP.hpp
@@ -411,28 +411,89 @@ namespace LibLoader::LoaderApi {
 #endif
 } // namespace LibLoader::LoaderApi
 #endif //MIRAICP_PRO_LOADERAPIINTERNAL_H
-// Copyright (c) 2022. Eritque arcus and contributors.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or any later version(in your opinion).
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-#include <iostream>
-#include <sstream>
-#ifdef MIRAICP_LIB_LOADER
-#include "LoaderLogger.h"
-#define log(x) LibLoader::logger.info(x)
-#define err(x) LibLoader::logger.error(x)
-#else
+#ifndef MIRAICP_PRO_BOT_H
+#define MIRAICP_PRO_BOT_H
+#include <string>
+#include <vector>
+// #include "MiraiDefs.h"
+#ifndef MIRAICP_PRO_MIRAIDEFS_H
+#define MIRAICP_PRO_MIRAIDEFS_H
+// #define MiraiCPThrow(x) throw x.append(__FILE__, __LINE__)
+#define ErrorHandle(x, y) ErrorHandle0(__FILE__, __LINE__, (x), (y))
+#if defined(_MSC_VER)
+#define ShouldNotUse(msg) _Pragma("warning(error:4996)") [[deprecated(msg)]] _Pragma("warning(warning:4996)")
+#else // MSVC
+#if defined(__GNUC__)
+#define ShouldNotUse(msg) [[deprecated(msg)]] __attribute__((error(msg)))
+#else // GUNC
+#define ShouldNotUse(msg)
+#endif // ShouldNotUse
+#endif
+#include <string>
+namespace MiraiCP {
+    using QQID = unsigned long long;
+} // namespace MiraiCP
+#endif //MIRAICP_PRO_MIRAIDEFS_H
+namespace MiraiCP {
+    class Friend;  // forward declaration
+    class Group;   // forward declaration
+    class Contact; // forward declaration
+    /// 当前bot账号信息
+    class Bot {
+    private:
+        bool inited = false;
+        std::string _nick;
+        std::string _avatarUrl;
+    public:
+        /// 该botid
+        QQID id;
+    private:
+        void check() {
+            if (!this->inited) {
+                refreshInfo();
+                this->inited = true;
+            }
+        }
+    public:
+        /*!
+         * @brief 刷新bot信息
+         * @param env
+         */
+        void refreshInfo();
+        /// 用id构建机器人
+        explicit Bot(QQID i) : id(i) {}
+        /// 取好友
+        Friend getFriend(QQID i) const;
+        /// 取群聊
+        Group getGroup(QQID groupid) const;
+        /// 昵称
+        std::string nick() {
+            check();
+            return this->_nick;
+        }
+        /// 头像下载链接
+        std::string avatarUrl() {
+            check();
+            return this->_avatarUrl;
+        }
+        /// 取好友列表
+        std::vector<QQID> getFriendList() const;
+        /// 好友列表string形式返回，利于保存
+        std::string FriendListToString() const;
+        /// 取群列表
+        std::vector<QQID> getGroupList() const;
+        /// 群列表string形式返回，利于保存
+        std::string GroupListToString() const;
+        bool operator==(const Contact &c) const;
+        bool operator==(const Bot &b) const {
+            return this->id == b.id;
+        }
+    };
+} // namespace MiraiCP
+#endif //MIRAICP_PRO_BOT_H
+#ifndef MIRAICP_PRO_CPPPLUGIN_H
+#define MIRAICP_PRO_CPPPLUGIN_H
+#include <utility>
 // #include "Logger.h"
 #ifndef MIRAICP_PRO_LOGGER_H
 #define MIRAICP_PRO_LOGGER_H
@@ -501,24 +562,6 @@ namespace MiraiCP {
 } // namespace MiraiCP
 #endif //MIRAICP_PRO_MIRAICODE_H
 // #include "MiraiDefs.h"
-#ifndef MIRAICP_PRO_MIRAIDEFS_H
-#define MIRAICP_PRO_MIRAIDEFS_H
-// #define MiraiCPThrow(x) throw x.append(__FILE__, __LINE__)
-#define ErrorHandle(x, y) ErrorHandle0(__FILE__, __LINE__, (x), (y))
-#if defined(_MSC_VER)
-#define ShouldNotUse(msg) _Pragma("warning(error:4996)") [[deprecated(msg)]] _Pragma("warning(warning:4996)")
-#else // MSVC
-#if defined(__GNUC__)
-#define ShouldNotUse(msg) [[deprecated(msg)]] __attribute__((error(msg)))
-#else // GUNC
-#define ShouldNotUse(msg)
-#endif // ShouldNotUse
-#endif
-#include <string>
-namespace MiraiCP {
-    using QQID = unsigned long long;
-} // namespace MiraiCP
-#endif //MIRAICP_PRO_MIRAIDEFS_H
 #include <functional>
 #include <sstream>
 namespace MiraiCP {
@@ -628,132 +671,6 @@ namespace MiraiCP {
     };
 } // namespace MiraiCP
 #endif //MIRAICP_PRO_LOGGER_H
-#define log(x) MiraiCP::Logger::logger.info(x)
-#define err(x) MiraiCP::Logger::logger.error(x)
-#endif
-// todo(antares): 不要将上述宏定义放在这里；需要用到宏的地方转移到cpp文件中
-#ifndef MIRAICP_PRO_REDIRECTCOUT_H
-#define MIRAICP_PRO_REDIRECTCOUT_H
-namespace MiraiCP {
-    class OStreamRedirector {
-    public:
-        ~OStreamRedirector() {
-            obj.rdbuf(old);
-        }
-        /**
-         * @brief 重定向 obj 的流
-         * @param obj 需要重定向的流
-         * @param new_buffer 重定向到的新缓冲区
-         */
-        explicit OStreamRedirector(std::ostream &obj, std::streambuf *newBuffer)
-            : old(obj.rdbuf(newBuffer)), obj(obj) {}
-    private:
-        // 被重定向的流
-        std::ostream &obj; // todo(antares): 不要在类成员中使用引用；改为指针
-        // 旧的缓冲区目标
-        std::streambuf *old;
-    };
-    // todo(antares): 整理格式，成员变量在最前，之后构造、析构函数，最后成员函数
-    //  继承了具有虚析构函数的父类，析构函数请显式写为virtual override
-    //  长度较小的成员（bool）放到后面
-    class OString : private std::streambuf, public std::ostream {
-    public:
-        // 输出是否为 info 级别
-        explicit OString(bool info) : std::ostream(this), info(info) {}
-    private:
-        bool info;
-        // 缓冲区
-        std::ostringstream result{};
-        // 加入缓冲区
-        int overflow(std::streambuf::int_type c) override {
-            if (c == EOF)
-                out();
-            else
-                result.put((std::streambuf::char_type) c);
-            return c;
-        }
-        // std::streambuff 的 sync
-        int sync() override {
-            out();
-            return 0;
-        }
-        // 输出缓冲区内容, 相当于 flush
-        void out() {
-            if (info)
-                log(result.str());
-            else
-                err(result.str());
-            result.str("");
-        }
-    };
-} // namespace MiraiCP
-#endif //MIRAICP_PRO_REDIRECTCOUT_H
-#ifndef MIRAICP_PRO_BOT_H
-#define MIRAICP_PRO_BOT_H
-#include <string>
-#include <vector>
-// #include "MiraiDefs.h"
-namespace MiraiCP {
-    class Friend;  // forward declaration
-    class Group;   // forward declaration
-    class Contact; // forward declaration
-    /// 当前bot账号信息
-    class Bot {
-    private:
-        bool inited = false;
-        std::string _nick;
-        std::string _avatarUrl;
-    public:
-        /// 该botid
-        QQID id;
-    private:
-        void check() {
-            if (!this->inited) {
-                refreshInfo();
-                this->inited = true;
-            }
-        }
-    public:
-        /*!
-         * @brief 刷新bot信息
-         * @param env
-         */
-        void refreshInfo();
-        /// 用id构建机器人
-        explicit Bot(QQID i) : id(i) {}
-        /// 取好友
-        Friend getFriend(QQID i) const;
-        /// 取群聊
-        Group getGroup(QQID groupid) const;
-        /// 昵称
-        std::string nick() {
-            check();
-            return this->_nick;
-        }
-        /// 头像下载链接
-        std::string avatarUrl() {
-            check();
-            return this->_avatarUrl;
-        }
-        /// 取好友列表
-        std::vector<QQID> getFriendList() const;
-        /// 好友列表string形式返回，利于保存
-        std::string FriendListToString() const;
-        /// 取群列表
-        std::vector<QQID> getGroupList() const;
-        /// 群列表string形式返回，利于保存
-        std::string GroupListToString() const;
-        bool operator==(const Contact &c) const;
-        bool operator==(const Bot &b) const {
-            return this->id == b.id;
-        }
-    };
-} // namespace MiraiCP
-#endif //MIRAICP_PRO_BOT_H
-#ifndef MIRAICP_PRO_CPPPLUGIN_H
-#define MIRAICP_PRO_CPPPLUGIN_H
-#include <utility>
-// #include "Logger.h"
 // #include "PluginConfig.h"
 namespace MiraiCP {
     /// 插件父类
@@ -5084,6 +5001,3 @@ MIRAICP_EXPORT void FUNC_EXIT();
 MIRAICP_EXPORT const MiraiCP::PluginConfig &PLUGIN_INFO();
 }
 #endif //MIRAICP_PRO_UTILS_H
-
-#undef log
-#undef err
