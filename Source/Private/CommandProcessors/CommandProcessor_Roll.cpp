b447f090179b0023c2877a91260408286b3f30cb
Luo <hl1282456555@outlook.com>
2022-08-14 14:55:07 +0800

    Update CommandProcessor_Roll.cpp

--- Source/Private/CommandProcessors/CommandProcessor_Roll.cpp
+++ Source/Private/CommandProcessors/CommandProcessor_Roll.cpp
@@ -34,34 +34,31 @@ void FCommandProcessor_Roll::ProcessMessageCommand(const std::shared_ptr<MiraiCP
 
 	std::vector<std::string> spaceResult = UGenericStringUtils::SplitIntoArray(Params, " ");
 
-	if (spaceResult.empty())
+	if (!spaceResult.empty())
 	{
-		Event->group.quoteAndSendMessage(MiraiCP::PlainText(helpText), Event->message.source.value());
-		return;
-	}
+		std::vector<std::string> result = UGenericStringUtils::SplitIntoArray(spaceResult[0], "-");
 
-	std::vector<std::string> result = UGenericStringUtils::SplitIntoArray(spaceResult[0], "-");
+		Event->botlogger.info("result: ", result.size());
 
-	Event->botlogger.info("result: ", result.size());
-
-	if (result.size() != 2)
-	{
-		for (int Index = 0; Index < result.size(); ++Index)
+		if (result.size() != 2)
 		{
-			if (std::count_if(result[Index].cbegin(), result[Index].cend(), [](unsigned char Value) { return !std::isdigit(Value);}) > 0)
+			for (int Index = 0; Index < result.size(); ++Index)
 			{
-				Event->group.quoteAndSendMessage(MiraiCP::PlainText(helpText), Event->message.source.value());
-				return;
+				if (std::count_if(result[Index].cbegin(), result[Index].cend(), [](unsigned char Value) { return !std::isdigit(Value); }) > 0)
+				{
+					Event->group.quoteAndSendMessage(MiraiCP::PlainText(helpText), Event->message.source.value());
+					return;
+				}
 			}
-		}
 
-		randomMin = boost::lexical_cast<int>(result.at(0));
-		randomMax = boost::lexical_cast<int>(result.at(1));
-	}
-	else
-	{
-		Event->group.quoteAndSendMessage(MiraiCP::PlainText(helpText), Event->message.source.value());
-		return;
+			randomMin = boost::lexical_cast<int>(result.at(0));
+			randomMax = boost::lexical_cast<int>(result.at(1));
+		}
+		else
+		{
+			Event->group.quoteAndSendMessage(MiraiCP::PlainText(helpText), Event->message.source.value());
+			return;
+		}
 	}
 
 	boost::random::uniform_int_distribution<> dist(randomMin, randomMax);
